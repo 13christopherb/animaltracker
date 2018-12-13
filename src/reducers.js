b02d5fd1ea404dd1f1778bfc0404ed4d4b3843c9
Chris Berglund <13christopherb@gmail.com>
2018-12-13 10:53:21 -0800

    Clear form fields for transport form after submission

--- src/reducers.js
+++ src/reducers.js
@@ -2,6 +2,7 @@ import {combineReducers} from 'redux';
 import {reducer as formReducer} from 'redux-form'
 import animalsReducer from './app/animals/ducks';
 import {animalFormTypes} from './app/animal-forms/ducks'
+import {transportFormTypes} from "./app/transport-form/ducks";
 import authenticationReducer from './app/authentication/ducks';
 import locationReducer from './app/locations/ducks';
 import transportsReducer from './app/transports/ducks';
@@ -12,10 +13,18 @@ export default combineReducers({
     locations: locationReducer,
     transports: transportsReducer,
     form: formReducer.plugin({
-        AnimalForm: (state, action) => { // <------ 'account' is name of form given to reduxForm()
+        AnimalForm: (state, action) => {
             switch (action.type) {
                 case animalFormTypes.ADD_ANIMAL_SUCCESS:
-                    return undefined;       // <--- blow away form data
+                    return undefined;
+                default:
+                    return state;
+            }
+        },
+        TransportForm: (state, action) => {
+            switch (action.type) {
+                case transportFormTypes.ADD_TRANSPORT_SUCCESS:
+                    return undefined;
                 default:
                     return state;
             }
